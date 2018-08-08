6bc07eebb31b27119b2e3135a9f95e21c53591e3
YingqiTao <yingqitao2000@gmail.com>
2018-08-08 14:04:05 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,36 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time >= 11
+    puts "It is time for bed."
 end
 
 def curfew_checker(time)
-  # code goes here
+  if time >= 11
+    puts "It's bed time!"
+  else
+    puts "You have time to play!"
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time < 6
+    puts "Fun time!"
+    elsif time >= 11
+    puts "It's bed time!"
+  else
+    puts "The night is young :)"
 end
 
+curfew = 11
+
 def deluxe_curfew_checker(time)
-  # code goes here
+  if time >= 11
+    puts "It's bed time."
+  else
+    puts "You still have " + curfew-time + " hours."
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  if current_time >= curfew_time
+    puts "It's bed time."
+  else
+    puts "You still have " + curfew_time-current_time
 end
